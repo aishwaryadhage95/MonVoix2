086850a1dd865c156e76eaf8cad52fec447cc049
aishwarya.dhage95 <aishwarya.dhage@ves.ac.in>
2017-03-04 20:29:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aishwarya/monvoix/tab1ttg.java
@@ -0,0 +1,23 @@
+package com.example.aishwarya.monvoix;
+
+/**
+ * Created by aishwarya on 19/02/17.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class tab1ttg extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1ttg, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/aishwarya/monvoix/tab2gtt.java
@@ -0,0 +1,23 @@
+package com.example.aishwarya.monvoix;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by aishwarya on 19/02/17.
+ */
+
+public class tab2gtt extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2gtt, container, false);
+
+        return rootView;
+    }
+}
+
